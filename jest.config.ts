a3ddc29a06d7328a9c8e8c81ead49239fa8ec70f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-02 10:01:59 -0300

    :gear: update: config - jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@
  * https://jestjs.io/docs/configuration
  */
 
+import { pathsToModuleNameMapper } from "ts-jest";
+import {compilerOptions} from "./tsconfig.json"
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -20,7 +23,7 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "<rootDir>/test/**/*.ts"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
@@ -88,7 +91,10 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  //moduleNameMapper: {
+  //  "@/(.*)": "<rootDir>/src/$1",
+  //},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */)
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -124,9 +130,7 @@ export default {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: ["<rootDir>/src", "<rootDir>/test"],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -144,7 +148,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jest-environment-node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -173,7 +177,9 @@ export default {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
@@ -192,4 +198,4 @@ export default {
 
   // Whether to use watchman for file crawling
   // watchman: true,
-};
+}
