f8c68ea46729c99cd3557d91e8e2646b1f9b02c7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-02 10:52:02 -0300

    :construction_worker: build: project

---
+++ build/src/server.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("app");
+const PORT = process.env.PORT;
+app_1.app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
