f56e5c49cfe8a2a3351a6881077de05f9dbcf19a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-02 15:10:18 -0300

    :sparkles: | create: entity - User (entity)

---
+++ src/entity/User.ts
@@ -0,0 +1,15 @@
+export class User {
+  id?: string
+  name: string
+  username: string
+  email: string
+
+  private constructor({ name, username, email }: User) {
+    return Object.assign(this, { name, username, email })
+  }
+
+  static create({ username, email, name }: User) {
+    const user = new User({ username, name, email })
+    return user
+  }
+}
