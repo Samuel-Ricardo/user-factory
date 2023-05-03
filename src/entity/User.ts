c81b23fae8fa5e92163a97fc0fd013d6c68bdd86
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-03 09:28:38 -0300

    :sparkles: | update: entitiy - User

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,14 +1,16 @@
+import { IUserData } from "@Types"
+
 export class User {
   id?: string
-  name: string
-  username: string
-  email: string
+  name!: string
+  username!: string
+  email!: string
 
   private constructor({ name, username, email }: User) {
     return Object.assign(this, { name, username, email })
   }
 
-  static create({ username, email, name }: User) {
+  static create({ username, email, name }: IUserData) {
     const user = new User({ username, name, email })
     return user
   }
