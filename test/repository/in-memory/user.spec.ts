15706cce0c0096061031e3fd2db66e372e3d8f2c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-03 10:39:21 -0300

    :white_check_mark: | create: unit test - user exists (repository)

--- test/repository/in-memory/user.spec.ts
+++ test/repository/in-memory/user.spec.ts
@@ -1,4 +1,4 @@
-import { VALID } from "@config"
+import { VALID } from "@config/consts"
 import { IUsersRepository } from "@/repository/IUserRepository"
 import { UserRespositoryInMemory } from "@/repository/in-memory/UserRepositoryInMemory"
 import { User } from "@entity"
@@ -13,8 +13,6 @@ describe("In Memory User repository test", () => {
   it("Should save user sucessfuly", async () => {
     const user = await repository.create(VALID.USER)
 
-    console.log(user)
-
     expect(user).toBeInstanceOf(User)
     expect(user).toHaveProperty("id")
     expect(user.name).toEqual(VALID.USER.name)
@@ -22,7 +20,7 @@ describe("In Memory User repository test", () => {
 
   it("User should exists", async () => {
     const user = await repository.create(VALID.USER)
-    const exists = await repository.exist(user.name)
+    const exists = await repository.exist(user.username)
 
     expect(exists).toBeTruthy()
   })
--- test/repository/prisma/user.spec.ts
+++ test/repository/prisma/user.spec.ts
@@ -5,6 +5,7 @@
 import { IUsersRepository } from "@/repository/IUserRepository"
 import { PrismaUserRepository } from "@/repository/prisma/PrismaUserRepository"
 import { VALID } from "@config/consts"
+import { User } from "@entity"
 
 describe("Prisma User repository test", () => {
   let repository: IUsersRepository
@@ -13,12 +14,25 @@ describe("Prisma User repository test", () => {
     repository = new PrismaUserRepository()
   })
 
+  afterEach(() => {
+    VALID.USER = {
+      name: VALID.USER.name + Math.random(),
+      username: VALID.USER.username + Math.random(),
+      email: VALID.USER.email + Math.random(),
+    }
+  })
+
   it("It Should Save Successfully", async () => {
     const user = await repository.create(VALID.USER)
 
-    console.log(user)
-
     expect(user).toHaveProperty("id")
     expect(user.name).toEqual(VALID.USER.name)
+  })
+
+  it("User Should Exists", async () => {
+    const user = await repository.create(VALID.USER)
+    const exists = await repository.exist(user.username)
+
+    expect(exists).toBeTruthy()
   })
 })
