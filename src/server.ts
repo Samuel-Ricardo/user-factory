24695528315d45b15d86f8e4e1254aa8a5e1bf88
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-03 09:35:33 -0300

    :sparkles: | update: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app"
 import { ENV } from "@config"
 
-const PORT = ENV.PORT
+const PORT = ENV.PORT()
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
