55428e8ac6b135bdd13ae2a3f0cd1789e8b5a27f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-04 21:56:56 -0300

    [:gear:] | update: config - errors

--- src/config/errors.ts
+++ src/config/errors.ts
@@ -1,5 +1,13 @@
-export class UserAlredyExists extends Error {
-  constructor() {
-    super("User alredy exists")
-  }
+import { IError, IResponse } from "@Types"
+
+export const UserAlredyExists: IResponse<IError> = {
+  error: true,
+  status: 400,
+  data: { message: "User alredy exists" },
 }
+
+//export class UserAlredyExists extends Error {
+//  constructor() {
+//    super("User alredy exists")
+//  }
+//}
