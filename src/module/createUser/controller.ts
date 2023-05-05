77fba2c8eb009fc517aa85c4d91574e00f44b8fd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-04 22:07:51 -0300

    [:sparkles:] | create: controller -  create user

--- src/module/createUser/controller.ts
+++ src/module/createUser/controller.ts
@@ -8,6 +8,8 @@ export class CreateUserController {
     const { name, username, email } = req.body
     const user = await this.service.execute({ name, username, email })
 
+    if (user.error) return res.status(user.status).json({ response: user })
+
     return res.status(201).json({ user })
   }
 }
