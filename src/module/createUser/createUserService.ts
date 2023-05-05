092d9ad996a14b6a014b3f661f2ba0499fbfec72
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-04 22:07:27 -0300

    [:sparkles:] | create: service -  create user

--- src/module/createUser/createUserService.ts
+++ src/module/createUser/createUserService.ts
@@ -1,18 +1,23 @@
 import { IUsersRepository } from "@/repository/IUserRepository"
-import { IUserData } from "@Types"
+import { IError, IResponse, IUserData } from "@Types"
 import { User } from "@entity"
 import { ERROR } from "@config"
 
 export class CreateUserService {
   constructor(private repository: IUsersRepository) {}
 
-  async execute({ name, username, email }: IUserData) {
+  async execute({
+    name,
+    username,
+    email,
+  }: IUserData): Promise<IResponse<IUserData | IError>> {
     if (await this.repository.exist(username)) {
-      throw new ERROR.UserAlredyExists()
+      return ERROR.UserAlredyExists
     }
 
     const model = User.create({ name, username, email })
     const user = await this.repository.create(model)
-    return user
+
+    return { error: false, status: 201, data: user } as IResponse<IUserData>
   }
 }
