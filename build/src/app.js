bbcbcc949b0b15791a6e7aeaabb7488b2ff7fc84
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-04 23:20:48 -0300

    [:construction_worker:] - build: project

--- build/src/app.js
+++ build/src/app.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
-const errors_1 = require("./middleware/errors");
 const routes_1 = require("./routes");
+const _middleware_1 = require("./middleware/index.js");
+require("dotenv/config");
 const app = (0, express_1.default)();
 exports.app = app;
 app.use(express_1.default.json());
-app.use(errors_1.errorHandler);
 app.use(routes_1.routes);
+app.use(_middleware_1.errorHandler);
